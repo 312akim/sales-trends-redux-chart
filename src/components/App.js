bee796a29c0105dd4af56fcd5cf2a3e98883f33b
Alex Kim <312akim@gmail.com>
2021-09-15 16:29:24 -0700

    Setup: Initial project setup

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div style={styles.wrapper}>
+      <header className="App-header">
+        Sales Trends Chart and Form
+      </header>
+      <body>
+        Body
+      </body>
+    </div>
+  );
+}
+
+const styles = {
+  wrapper: {
+    background: 'gray',
+    height: '100vh'
+  }
+}
+
+export default App;
